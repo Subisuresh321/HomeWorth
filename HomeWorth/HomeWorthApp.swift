7d3a7e7f7914f380f4c0541b273142f4757499ac
Subisuresh321 <subisuresh69@gmail.com>
2025-08-08 09:46:10 +0530

    Initial Commit

---
+++ HomeWorth/HomeWorthApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWorthApp.swift
+//  HomeWorth
+//
+//  Created by Subi Suresh on 08/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct HomeWorthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HomeWorthTests/HomeWorthTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWorthTests.swift
+//  HomeWorthTests
+//
+//  Created by Subi Suresh on 08/08/2025.
+//
+
+import Testing
+@testable import HomeWorth
+
+struct HomeWorthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
